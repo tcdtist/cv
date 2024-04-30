3173dc24ca554e2e72d8908c1f01f709ae05b45e
tcdtist <chithanh.tist@gmail.com>
2024-04-30 17:53:12 +0700

    code push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Section } from "@/components/ui/section";
+import { API_ROOT } from "@/constants";
 import { RESUME_DATA } from "@/data/resume-data";
 import { GlobeIcon, MailIcon, PhoneIcon } from "lucide-react";
 import { Metadata } from "next";
@@ -18,93 +19,110 @@ export const metadata: Metadata = {
   },
 };
 
-export default function Page() {
+import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
+
+const SOCIALS = {
+  "GitHubIcon":GitHubIcon,
+  "LinkedInIcon":LinkedInIcon,
+  "XIcon":XIcon,
+}
+
+export default async function Page() {
+
+   const response = await fetch(`${API_ROOT}me/cv/resume.json`)
+   const data = await response.json()
+   console.log(data);
+   
   return (
     <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 md:p-16">
       <section className="mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-6">
         <div className="flex items-center justify-between gap-2">
           <div className="flex-1 space-y-1.5">
-            <h1 className="text-2xl font-bold">{RESUME_DATA.name}</h1>
+            <h1 className="text-2xl font-bold">{data.name}</h1>
             <p className="max-w-md text-pretty font-mono text-sm text-muted-foreground">
-              {RESUME_DATA.about}
+              {data.about}
             </p>
             <p className="max-w-md items-center text-pretty font-mono text-xs text-muted-foreground">
               <a
                 className="inline-flex gap-x-1.5 align-baseline leading-none hover:underline"
-                href={RESUME_DATA.locationLink}
+                href={data.locationLink}
                 target="_blank"
               >
                 <GlobeIcon className="size-3" />
-                {RESUME_DATA.location}
+                {data.location}
               </a>
             </p>
             <div className="flex gap-x-1 pt-1 font-mono text-sm text-muted-foreground print:hidden">
-              {RESUME_DATA.contact.email ? (
+              {data.contact.email ? (
                 <Button
                   className="size-8"
                   variant="outline"
                   size="icon"
                   asChild
                 >
-                  <a href={`mailto:${RESUME_DATA.contact.email}`}>
+                  <a href={`mailto:${data.contact.email}`}>
                     <MailIcon className="size-4" />
                   </a>
                 </Button>
               ) : null}
-              {RESUME_DATA.contact.tel ? (
+              {data.contact.tel ? (
                 <Button
                   className="size-8"
                   variant="outline"
                   size="icon"
                   asChild
                 >
-                  <a href={`tel:${RESUME_DATA.contact.tel}`}>
+                  <a href={`tel:${data.contact.tel}`}>
                     <PhoneIcon className="size-4" />
                   </a>
                 </Button>
               ) : null}
-              {RESUME_DATA.contact.social.map((social) => (
-                <Button
-                  key={social.name}
-                  className="size-8"
-                  variant="outline"
-                  size="icon"
-                  asChild
-                >
-                  <a href={social.url}>
-                    <social.icon className="size-4" />
-                  </a>
-                </Button>
-              ))}
+              {data.contact.social.map((social) => {
+                const SocialIcon = SOCIALS[social.icon]
+                
+                return <Button
+                key={social.name}
+                className="size-8"
+                variant="outline"
+                size="icon"
+                asChild
+              >
+                <a href={social.url}>
+                  <SocialIcon icon={social.icon} className="size-4" />
+                </a>
+              </Button>
+              }
+                
+              )}
             </div>
             <div className="hidden flex-col gap-x-1 font-mono text-sm text-muted-foreground print:flex">
-              {RESUME_DATA.contact.email ? (
-                <a href={`mailto:${RESUME_DATA.contact.email}`}>
-                  <span className="underline">{RESUME_DATA.contact.email}</span>
+              {data.contact.email ? (
+                <a href={`mailto:${data.contact.email}`}>
+                  <span className="underline">{data.contact.email}</span>
                 </a>
               ) : null}
-              {RESUME_DATA.contact.tel ? (
-                <a href={`tel:${RESUME_DATA.contact.tel}`}>
-                  <span className="underline">{RESUME_DATA.contact.tel}</span>
+              {data.contact.tel ? (
+                <a href={`tel:${data.contact.tel}`}>
+                  <span className="underline">{data.contact.tel}</span>
                 </a>
               ) : null}
             </div>
           </div>
 
           <Avatar className="size-36">
-            <AvatarImage alt={RESUME_DATA.name} src={RESUME_DATA.avatarUrl} />
-            <AvatarFallback>{RESUME_DATA.initials}</AvatarFallback>
+            <AvatarImage alt={data.name} src={data.avatarUrl} />
+            <AvatarFallback>{data.initials}</AvatarFallback>
           </Avatar>
         </div>
         <Section>
           <h2 className="text-xl font-bold">About</h2>
           <p className="text-pretty font-mono text-sm text-muted-foreground">
-            {RESUME_DATA.summary}
+            {data.summary}
           </p>
         </Section>
         <Section>
           <h2 className="text-xl font-bold">Work Experience</h2>
-          {RESUME_DATA.work.map((work) => {
+          {data.work.map((work) => {
             return (
               <Card key={work.company}>
                 <CardHeader>
@@ -149,7 +167,7 @@ export default function Page() {
         </Section>
         <Section>
           <h2 className="text-xl font-bold">Education</h2>
-          {RESUME_DATA.education.map((education) => {
+          {data.education.map((education) => {
             return (
               <Card key={education.school}>
                 <CardHeader>
@@ -170,7 +188,7 @@ export default function Page() {
         <Section>
           <h2 className="text-xl font-bold">Skills</h2>
           <div className="flex flex-wrap gap-1">
-            {RESUME_DATA.skills.map((skill) => {
+            {data.skills.map((skill) => {
               return <Badge key={skill}>{skill}</Badge>;
             })}
           </div>
@@ -179,7 +197,7 @@ export default function Page() {
         <Section className="scroll-mb-16">
           <h2 className="text-xl font-bold">Projects</h2>
           <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
-            {/* {RESUME_DATA.projects.map((project) => {
+            {/* {data.projects.map((project) => {
               return (
                 <ProjectCard
                   key={project.title}
@@ -190,7 +208,7 @@ export default function Page() {
                 />
               );
             })} */}
-            {RESUME_DATA.projects.length === 0 ? (
+            {data.projects.length === 0 ? (
               <CardContent className="mx-3 mt-2">Updating...</CardContent>
             ) : null}
           </div>
@@ -200,10 +218,10 @@ export default function Page() {
       <CommandMenu
         links={[
           {
-            url: RESUME_DATA.personalWebsiteUrl,
+            url: data.personalWebsiteUrl,
             title: "Personal Website",
           },
-          ...RESUME_DATA.contact.social.map((socialMediaLink) => ({
+          ...data.contact.social.map((socialMediaLink) => ({
             url: socialMediaLink.url,
             title: socialMediaLink.name,
           })),
---
+++ src/constants/api.ts
@@ -0,0 +1,7 @@
+export const API_ROOT = process.env.NEXT_PUBLIC_HOST
+export const TIMEOUT = 15000
+
+export default {
+    API_ROOT,
+    TIMEOUT
+} as const
